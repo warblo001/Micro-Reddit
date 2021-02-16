6702f34e2ef4a48120c7a83c36dafcc29ca56e8c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-16 12:02:00 +0100

    Add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :username, length: {minimum: 4, maximum: 12}, presence: true, uniqueness: true
+	validates :email, uniqueness: true
+	validates :password, length: {minimum: 6, maximum: 16}, presence: true
 end
